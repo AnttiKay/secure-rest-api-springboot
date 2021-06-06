482dc51d1bb4c96ed3dea41693b398eb91183876
AnttiKayhko <antti.kayhko@metropolia.fi>
2021-06-06 23:13:21 +0300

    first commit

---
+++ src/main/java/com/example/securerestapispringboot/SecureRestApiSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.securerestapispringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureRestApiSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureRestApiSpringbootApplication.class, args);
+	}
+
+}
